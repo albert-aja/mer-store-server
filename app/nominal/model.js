d34d76941147699abf7352019ff7e967938a1f31
Albert <albertaries18@gmail.com>
2022-01-19 10:55:26 +0700

    190122 - admin nominal

---
+++ app/nominal/model.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+let nominalSchema = mongoose.Schema({
+  coinQuantity: {
+    type: Number,
+    default: 0,
+  },
+  price: {
+    type: Number,
+    default: 0,
+  },
+  coinName: {
+    type: String,
+    require: [true, "Nama Koin harus diisi"],
+  },
+});
+
+module.exports = mongoose.model("Nominal", nominalSchema);
